daf265472e7af3ed2706914fb924718e951e9c12
AfaqCoding <afaqcoder@gmail.com>
2025-02-06 00:47:26 -0800

    blog page and search input add

--- src/app/components/sections/BlogSections/BlogCards.jsx
+++ src/app/components/sections/BlogSections/BlogCards.jsx
@@ -3,10 +3,10 @@ import React from "react";
 function BlogCards() {
   return (
     <>
-      <div className="flex gap-8">
-        <div className="w-[320px] h-[300px] border border-gray-100 rounded-xl shadow-xl"></div>
-        <div className="w-[320px] h-[300px] border border-gray-100 rounded-xl shadow-xl"></div>
-        <div className="w-[320px] h-[300px] border border-gray-100 rounded-xl shadow-xl"></div>
+      <div className="flex gap-8 px-20">
+        <div className="w-[20vw] h-[300px] border border-gray-100 rounded-xl shadow-xl"></div>
+        <div className="w-[20vw] h-[300px] border border-gray-100 rounded-xl shadow-xl"></div>
+        <div className="w-[20vw] h-[300px] border border-gray-100 rounded-xl shadow-xl"></div>
       </div>
     </>
   );
