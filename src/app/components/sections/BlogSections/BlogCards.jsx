1f1a54f6bb850da5d5c6b19ece99f82d0261151e
AfaqCoding <afaqcoder@gmail.com>
2025-02-06 00:48:14 -0800

    blog page and search input add

--- src/app/components/sections/BlogSections/BlogCards.jsx
+++ src/app/components/sections/BlogSections/BlogCards.jsx
@@ -4,9 +4,9 @@ function BlogCards() {
   return (
     <>
       <div className="flex gap-8 px-20">
-        <div className="w-[20vw] h-[300px] border border-gray-100 rounded-xl shadow-xl"></div>
-        <div className="w-[20vw] h-[300px] border border-gray-100 rounded-xl shadow-xl"></div>
-        <div className="w-[20vw] h-[300px] border border-gray-100 rounded-xl shadow-xl"></div>
+        <div className="w-[22vw] h-[300px] border border-gray-100 rounded-xl shadow-xl"></div>
+        <div className="w-[22vw] h-[300px] border border-gray-100 rounded-xl shadow-xl"></div>
+        <div className="w-[22vw] h-[300px] border border-gray-100 rounded-xl shadow-xl"></div>
       </div>
     </>
   );
